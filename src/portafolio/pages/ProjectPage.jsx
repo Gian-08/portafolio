d7694121f5fb960937c6b6f7234e203f30850c06
gian-08 <gianmarcoespinozachavez62@gmail.com>
2024-05-09 14:50:17 -0500

    docs nuevo y mejoras en pagina proyectos

--- src/portafolio/pages/ProjectPage.jsx
+++ src/portafolio/pages/ProjectPage.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
-
+import imgcolegio1 from '../../assets/sistema-colegio/Screenshot_1.png'
+import imgcolegio2 from '../../assets/sistema-colegio/Screenshot_2.png'
+import imgcolegio3 from '../../assets/sistema-colegio/Screenshot_3.png'
+import imgcolegio4 from '../../assets/sistema-colegio/Screenshot_4.png'
+import imgcall1 from '../../assets/sistema-call-center/Screenshot_1.png'
+import imgcall2 from '../../assets/sistema-call-center/Screenshot_2.png'
+import imgcall3 from '../../assets/sistema-call-center/Screenshot_3.png'
+import imgcall4 from '../../assets/sistema-call-center/Screenshot_4.png'
+import imgcall5 from '../../assets/sistema-call-center/Screenshot_5.png'
+import imgconecta1 from '../../assets/Conecta-peru/Screenshot_1.png'
+import imgdigimedia1 from '../../assets/landing-digimedia/Screenshot_1.png'
+import imgdigimedia2 from '../../assets/landing-digimedia/Screenshot_2.png'
+import imgdigimedia3 from '../../assets/landing-digimedia/Screenshot_3.png'
+import imgcertificados1 from '../../assets/Certificados/Screenshot_1.png'
+import imgcertificados2 from '../../assets/Certificados/Screenshot_2.png'
+import imgcertificados3 from '../../assets/Certificados/Screenshot_3.png'
 export const ProjectPage = () => {
   return (
 
@@ -13,14 +28,18 @@ export const ProjectPage = () => {
               <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="/src/assets/sistema-colegio/Screenshot_2.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcolegio1}class="d-block w-100" alt="..."/>
                   </div>
                   <div class="carousel-item">
-                    <img src="/src/assets/sistema-colegio/Screenshot_1.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcolegio2} class="d-block w-100" alt="..."/>
                   </div>
                   <div class="carousel-item">
-                    <img src="/src/assets/sistema-colegio/Screenshot_3.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcolegio3} class="d-block w-100" alt="..."/>
                   </div>
+                  <div class="carousel-item">
+                    <img src={imgcolegio4} class="d-block w-100" alt="..."/>
+                  </div>
+                  
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -47,19 +66,19 @@ export const ProjectPage = () => {
               <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
-                    <img src="/src/assets/sistema-call-center/Screenshot_1.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcall1} class="d-block w-100" alt="..."/>
                   </div>
                   <div class="carousel-item">
-                    <img src="/src/assets/sistema-call-center/Screenshot_2.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcall2}  class="d-block w-100" alt="..."/>
                   </div>
                   <div class="carousel-item">
-                    <img src="/src/assets/sistema-call-center/Screenshot_3.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcall3}  class="d-block w-100" alt="..."/>
                   </div>
                   <div class="carousel-item">
-                    <img src="/src/assets/sistema-call-center/Screenshot_4.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcall4}  class="d-block w-100" alt="..."/>
                   </div>
                   <div class="carousel-item">
-                    <img src="/src/assets/sistema-call-center/Screenshot_5.png" class="d-block w-100" alt="..."/>
+                    <img src={imgcall5}  class="d-block w-100" alt="..."/>
                   </div>
                 </div>
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -84,7 +103,7 @@ export const ProjectPage = () => {
 {/*proyecto Conecta-Peru*/}
         <div className="col-sm-12 mb-3">
               <div class="card">
-            <img src="/src/assets/Conecta-peru/Screenshot_1.png" class="card-img-top" alt="proyectos"/>
+            <img src={imgconecta1} class="card-img-top" alt="proyectos"/>
             <div class="card-body">
               <h5 class="card-title">Sistema web de tiendas</h5>
               <p class="card-text">Tecnologias usadas: HTML, CSS, Bootstrap, PHP nativo, MySQL
